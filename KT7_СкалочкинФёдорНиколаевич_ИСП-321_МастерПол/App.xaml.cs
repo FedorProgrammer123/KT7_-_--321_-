f7c794274852a7309ce9504f2c8c4804df19a570
FedorProgrammer123 <fedorlohkin@gmail.com>
2024-11-06 17:22:10 +0300

    Добавьте файлы проекта.

---
+++ KT7_СкалочкинФёдорНиколаевич_ИСП-321_МастерПол/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace KT7_СкалочкинФёдорНиколаевич_ИСП_321_МастерПол
+{
+    /// <summary>
+    /// Логика взаимодействия для App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
